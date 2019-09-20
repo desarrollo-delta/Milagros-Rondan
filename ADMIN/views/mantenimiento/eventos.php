b5ef4aa6890f99b68021687c4b7872c9f97d1b17
omar <omar@gmail.com>
2019-09-20 18:50:23 -0500

    Eventos

--- ADMIN/views/mantenimiento/eventos.php
+++ ADMIN/views/mantenimiento/eventos.php
@@ -1,1 +1,13 @@
-<h1>Esto es eventos</h1>
+<div class="container-fluid">
+    <div class="">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Seleccionar
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="index.php?action=">Eventos</a>
+                <a class="dropdown-item" href="#">Detalle de los eventos</a>
+            </div>
+        </div>
+    </div>
+</div>
